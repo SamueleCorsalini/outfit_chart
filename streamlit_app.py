877d94293d2ce7db834f1d9934a8530c9f9353de
SamueleCorsalini <samuelecorsalini@gmail.com>
2025-07-04 15:25:58 +0000

    bug fux

--- streamlit_app.py
+++ streamlit_app.py
@@ -115,7 +115,7 @@ def main():
     # === Classifica Ultimo Giorno === 
     today = datetime.date.today()
     yesterday = (today - datetime.timedelta(days=1)).strftime('%Y-%m-%d')
-    show_top3(yesterday, top3_ws)
+    show_top3(yesterday, sheet)
 
     # === Classifica ===
     st.header("Classifica Generale")
